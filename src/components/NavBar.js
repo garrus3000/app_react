89afcc9dd13c6410c9309a417d2f3a134f1434fe
Eduardo Odriozola <eduardocorrientes@gmail.com>
2022-02-17 16:09:46 -0300

    Desafio NavBar.js 17/02

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const NavBar = () => {
+    return (
+        <header>
+            <h1 style={{color: "#D4AF37"}}>AddFuel</h1>
+            <nav>
+                <a href="#">Inicio</a>
+                <a href="#">Productos</a>
+                <a href="#">Nosotros</a>
+                <a href="#">Contacto</a>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
